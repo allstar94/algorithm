80ae05e7e5df450f11b5067600c42d91c1225fa8
alsltar94 <alsltar94@gmail.com>
2020-12-15 17:23:08 +0000

    배열 추가

--- diction.py
+++ diction.py
@@ -1,5 +1,5 @@
 fruit = ["사과", "사과", "바나나", "바나나", "딸기", " 키위", "복숭아", "복숭아", "복숭아"]
-
+print(fruit)
 d = {}
 
 for f in fruit:
@@ -7,7 +7,9 @@ for f in fruit:
     d[f] = d[f] + 1
   else:
     d[f] = 1
-
+print(d)
 sd = sorted(d.items(),key=lambda x: x[1], reverse = True)
 
 print(sd)
+for x,y in sd:
+  print(y)
